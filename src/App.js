4cb7fa724682e87544d2122f24fce8da0fa1f1c8
Kirill Sokolov <zzzzz0908@yandex.ua>
2020-06-07 18:55:45 +0300

    remove unused react templates

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import SalaryItems from "./components/salary-items";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  state = {
+    salaryItems: [
+      { id: 1, value: 0 },
+      { id: 2, value: 0 },
+      { id: 3, value: 0 },
+      { id: 4, value: 0 }
+    ]
+  };
+
+  render() {
+    return (
+      <div>
+        <main className="container">
+          <h1>Hello</h1>
+        </main>
+      </div>
+    );
+  }
+
 }
 
 export default App;
---
+++ src/components/salary-items.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import SalaryItem from "./salary-item";
+
+class SalaryItems extends React.Component {
+  render() {
+    return (
+      <div>
+
+      </div>
+    );
+  }
+}
+
+export default SalaryItems;
